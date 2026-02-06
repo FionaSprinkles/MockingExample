b5651156e55a98b109c8d02a2e5775366727105d
Fiona Friberg <fionafriberg@outlook.com>
2026-02-06 11:13:00 +0100

    Add parameterized test for invalid input

--- src/test/java/com/example/BookingSystemTest.java
+++ src/test/java/com/example/BookingSystemTest.java
@@ -5,11 +5,14 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.time.LocalDateTime;
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -35,5 +38,26 @@ class BookingSystemTest {
 
     @AfterEach
     void tearDown() {
+    }
+
+    @ParameterizedTest (name = "{0}")
+    @MethodSource ("shouldThrowExceptionIfInvalidBookingInput")
+    void shouldThrowException (
+        String comment,
+        String roomId,
+        LocalDateTime startTime,
+        LocalDateTime endTime
+        ){
+
+        assertThrows(IllegalArgumentException.class, () -> bookingSystem.bookRoom(roomId, startTime, endTime));
+    }
+    static Stream<Arguments> shouldThrowExceptionIfInvalidBookingInput() {
+        LocalDateTime now = LocalDateTime.now();
+
+        return Stream.of(
+                Arguments.of("No input in start time", "Standard" , null , now.plusHours(24)),
+                Arguments.of("No input in end time" , "Standard" , now , null ),
+                Arguments.of("RoomId is null" , null , now , now.plusHours(24))
+        );
     }
 }
