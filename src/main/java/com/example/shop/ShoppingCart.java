db503c00f3afab566710f5ca8c4e50ab09c92e5d
Fiona Friberg <fionafriberg@outlook.com>
2026-02-08 20:03:48 +0100

    Add method for adding item to shoppingCart

--- src/main/java/com/example/shop/ShoppingCart.java
+++ src/main/java/com/example/shop/ShoppingCart.java
@@ -1,6 +1,18 @@
 package com.example.shop;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class ShoppingCart {
 
+    private Set<String> items = new HashSet<String>();
+
+
+    public void addItem (String itemName){
+        items.add(itemName);
+    }
+    public boolean containsItem(String itemName){
+        return items.contains(itemName);
+    }
 
 }
