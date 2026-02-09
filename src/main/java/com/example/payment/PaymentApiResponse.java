2426929e962f6f5f6d0704d1e528eb06790fbb56
Fiona Friberg <fionafriberg@outlook.com>
2026-02-09 20:17:15 +0100

    Add initial code for PaymentApiResponse

--- src/main/java/com/example/payment/PaymentApiResponse.java
+++ src/main/java/com/example/payment/PaymentApiResponse.java
@@ -1,4 +1,14 @@
 package com.example.payment;
 
 public class PaymentApiResponse {
+    private final boolean success;
+
+    public PaymentApiResponse(boolean success) {
+        this.success = success;
+    }
+
+    public boolean isSuccess() {
+        return success;
+    }
+
 }
