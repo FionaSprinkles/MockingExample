368fcb447fc58b087cc06f4bcb85223516b6a42c
Fiona Friberg <fionafriberg@outlook.com>
2026-02-09 10:09:42 +0100

    Initial code for ShopItems

--- src/main/java/com/example/shop/ShopItems.java
+++ src/main/java/com/example/shop/ShopItems.java
@@ -1,4 +1,19 @@
 package com.example.shop;
 
 public class ShopItems {
+    private String itemName;
+    private double itemPrice;
+
+    public ShopItems(double itemPrice, String itemName) {
+        this.itemPrice = itemPrice;
+        this.itemName = itemName;
+    }
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public double getItemPrice() {
+        return itemPrice;
+    }
 }
